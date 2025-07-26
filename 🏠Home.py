33c83432416307119175d4f8762c1c3bfd4f8604
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2025-07-26 19:19:36 +0330

    Create 🏠Home.py

---
+++ 🏠Home.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+# --- Page Config: Tab Title & Icon ---
+st.set_page_config(
+    page_title="Any Inu: A Memecoin Powered By Axelar",
+    page_icon="https://raw.githubusercontent.com/axelarnetwork/axelar-configs/main/images/tokens/ai.svg",
+    layout="wide"
+)
+
+
+st.title("A Memecoin Powered By Axelar")
+
+st.markdown("""
+Any Inu ($AI) is a omnichain meme token powered by Axelar's Interchain Token Service. 
+""")
+
+# --- Display Image ---
+#st.image("https://i.postimg.cc/s2HY308v/axelar.jpg", caption="", use_container_width=True)
+
+st.markdown("""
+https://www.axelar.network/
+
+https://www.anyinu.xyz/
+
+https://x.com/axelar
+
+https://x.com/AnyInucoin
+
+""")
+
